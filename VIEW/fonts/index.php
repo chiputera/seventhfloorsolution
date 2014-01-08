0f1619a9c58a608555f4be2e4951a0d768ced25e
chiputera <chiputera@sflosolition.com>
2014-01-08 15:39:05 +0700

    first run :D

---
+++ VIEW/fonts/index.php
@@ -0,0 +1,7 @@
+<?php
+header('Location: ../index.php');
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+    if(!isset($_GET['page'])){
+        header('Location:VIEW/index.html');
+    }else{
+        echo "HAHAHA";
+    }
+?>
